de27fea71931a9e948e42d236abc1c095fa98d49
Sayeeram05 <sr6172@srmist.edu.in>
2025-07-06 23:17:09 +0530

    diagonal-difference

---
+++ Coding Platform/HackerRank/Problemsolving/diagonal-difference.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    s = [0,0]
+    L = len(arr)
+    for i in range(L):
+        s[0] += arr[i][i]
+        s[1] += arr[L-1-i][i]
+    return abs(s[0]-s[1])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
